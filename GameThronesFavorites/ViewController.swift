c51e0a98acf5a99ebb90088d7b70e71fae8e079b
Jerry Yuan <yhyuan@gmail.com>
2019-05-16 10:24:36 -0400

    Initial Commit

---
+++ GameThronesFavorites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameThronesFavorites
+//
+//  Created by Yinhuan Yuan on 5/16/19.
+//  Copyright © 2019 Jun Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
